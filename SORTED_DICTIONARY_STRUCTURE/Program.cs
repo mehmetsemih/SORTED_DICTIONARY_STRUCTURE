db3ca2a3c4dc4ef38d2c73ba202f23b60d836a68
mehmetsemih <mehmetsemihunal@gmail.com>
2024-07-25 17:36:29 +0300

    SORTED_DICTIONARY

--- SORTED_DICTIONARY_STRUCTURE/Program.cs
+++ SORTED_DICTIONARY_STRUCTURE/Program.cs
@@ -4,15 +4,21 @@
     {
         static void Main(string[] args)
         {
-           
+            //SORTED_DICTIONARY_DESCRIBING
 
+           var kitap_indeks=new SortedDictionary<string,List<int>>()
+           {
+               {"HMTL",new List<int> {1,2,3,4} },
+               {"CSS",new List<int>{40,50,60} },
+           };
 
+            kitap_indeks.Add("JAVASCRİPT",new List<int>() { 100,300,400});
 
-
-
-
-
-
+            foreach (var k in kitap_indeks)
+            {
+                Console.WriteLine($"{k.Key}");
+                k.Value.ForEach(x => Console.WriteLine(x));
+            }
             Console.ReadKey();
         }
     }
